a2b91202ab447ff37c2eec1e4d7533d475ce3135
Your Name <you@example.com>
2024-06-10 11:31:16 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Pdf from './Components/Pdf'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Pdf />
   </React.StrictMode>
 );
 
